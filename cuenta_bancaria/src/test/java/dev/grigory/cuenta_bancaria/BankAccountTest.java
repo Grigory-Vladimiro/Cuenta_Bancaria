21988c30708d6bcec9231ec621350d7dbf0d69b4
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-28 10:58:19 +0100

    test_deposit_increases_the_acc

--- cuenta_bancaria/src/test/java/dev/grigory/cuenta_bancaria/BankAccountTest.java
+++ cuenta_bancaria/src/test/java/dev/grigory/cuenta_bancaria/BankAccountTest.java
@@ -10,4 +10,10 @@ public class BankAccountTest {
         BankAccount account = new BankAccount(1000.0f, 0.05f);
         assertEquals(1000.0f, account.getBalance());
     }
+    @Test
+    public void testDepositIncreasesBalance() {
+        BankAccount account = new BankAccount(100.0f, 5.0f);
+        account.deposit(50.0f);
+        assertEquals(150.0f, account.getBalance());
+    }
 }
