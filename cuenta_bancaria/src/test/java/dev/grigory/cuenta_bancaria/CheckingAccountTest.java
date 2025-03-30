8e85d8b9e640adc78de0e24bf1721ec39eee774e
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-30 18:53:32 +0200

    test_deposit_decreases_overdraft

--- cuenta_bancaria/src/test/java/dev/grigory/cuenta_bancaria/CheckingAccountTest.java
+++ cuenta_bancaria/src/test/java/dev/grigory/cuenta_bancaria/CheckingAccountTest.java
@@ -12,4 +12,12 @@ public class CheckingAccountTest {
         assertEquals(0.0f, account.getBalance());
         assertEquals(300.0f, account.getOverdraft(), 0.001f);
     }
+    @Test
+    public void testDepositReducesOverdraft() {
+        CheckingAccount account = new CheckingAccount(500.0f, 5.0f);
+        account.withdraw(800.0f);
+        account.deposit(200.0f);
+        assertEquals(100.0f, account.getOverdraft(), 0.001f);
+        assertEquals(0.0f, account.getBalance(), 0.001f);
+    }
 }
