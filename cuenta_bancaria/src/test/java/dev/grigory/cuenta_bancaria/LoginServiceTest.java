c7f8984c0443c813c89694ebd9d6c17c68c67b19
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-30 21:24:43 +0200

    test_login_scanner_to_increase_overall_coverage

--- cuenta_bancaria/src/test/java/dev/grigory/cuenta_bancaria/LoginServiceTest.java
+++ cuenta_bancaria/src/test/java/dev/grigory/cuenta_bancaria/LoginServiceTest.java
@@ -3,6 +3,9 @@ package dev.grigory.cuenta_bancaria;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class LoginServiceTest {
     @Test
     public void testLoginSuccess() {
@@ -25,5 +28,18 @@ public class LoginServiceTest {
     @Test
     public void testInteractiveLoginWithScanner() {
         LoginService loginService = new LoginService();
+    }
+    @Test
+    public void testInteractiveLoginWithCorrectCredentials() {
+        String simulatedInput = "admin\n1234\n";
+        InputStream originalIn = System.in;
+            try {
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+        LoginService loginService = new LoginService();
+        boolean result = loginService.login();
+        assertTrue(result);
+            } finally {
+        System.setIn(originalIn);
+        }
     }
 }
