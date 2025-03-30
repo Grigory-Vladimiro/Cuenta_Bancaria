dab5832a04719c10feb454680cf20d861383c4e9
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-30 21:05:11 +0200

    feat_login_with_scanner&3attempts

--- cuenta_bancaria/src/main/java/dev/grigory/cuenta_bancaria/LoginService.java
+++ cuenta_bancaria/src/main/java/dev/grigory/cuenta_bancaria/LoginService.java
@@ -1,9 +1,32 @@
 package dev.grigory.cuenta_bancaria;
 
+import java.util.Scanner;
+
 public class LoginService {
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "1234";  
     public boolean login(String username, String password) {
         return USERNAME.equals(username) && PASSWORD.equals(password);
+    }
+    public boolean login() {
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+    int attempts = 0;
+
+    while (attempts < 3) {
+        System.out.print("Username: ");
+        String user = scanner.nextLine();
+        System.out.print("Password: ");
+        String pass = scanner.nextLine();
+        if (login(user, pass)) {
+            System.out.println("Access granted.");
+            return true;
+        } else {
+            attempts++;
+            System.out.println("Invalid credentials. Attempts left: " + (3 - attempts));
+        }
+        }
+        System.out.println("Too many failed attempts. Access denied.");
+        return false;
     }
 }
